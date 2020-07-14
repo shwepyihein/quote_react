4c1cccb8c36b775a8b5ef716d6c20cd6cabf7bca
shwepyihein <shwepyihein.12@gmail.com>
2020-07-14 23:43:09 +0630

    Updates

---
+++ precache-manifest.c90608500548152a14beaeedaa517276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbcf916acc74c9880ec5b1071396610d",
+    "url": "/quote_react/index.html"
+  },
+  {
+    "revision": "a01a0d04769c550c50f4",
+    "url": "/quote_react/static/css/main.e9ae7e0f.chunk.css"
+  },
+  {
+    "revision": "77f993b34845e47ca873",
+    "url": "/quote_react/static/js/2.f1ad9125.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote_react/static/js/2.f1ad9125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a01a0d04769c550c50f4",
+    "url": "/quote_react/static/js/main.5d2cace9.chunk.js"
+  },
+  {
+    "revision": "41bcfce80de7779eb13f",
+    "url": "/quote_react/static/js/runtime-main.cc8c58bf.js"
+  }
+]);
